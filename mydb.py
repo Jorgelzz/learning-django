3d5a528262997bee7e66eb62eab1c3d1f1b98cbb
Jorge Luiz <jorgeluiz2027@hotmail.com>
2025-07-01 16:30:34 -0300

    added a new layer to security

--- mydb.py
+++ mydb.py
@@ -1,17 +1,16 @@
+import os
 import mysql.connector
+import dotenv
 
 dataBase = mysql.connector.connect (
-    host = 'localhost',
-    user = 'root',
-    passwd = 'admin'
+    host = os.environ.get('HOST'),
+    user = os.environ.get('USER'),
+    passwd = os.environ.get('PASSWRD')
 )
 
 
 # preparando um objeto cursor
-
 cursorObject  = dataBase.cursor()
-
 # Criando o banco de dados
-
 cursorObject.execute('CREATE DATABASE JRG')
 
